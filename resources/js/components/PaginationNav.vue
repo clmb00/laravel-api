4e6889020c5ef0227203e5c7093766e81482cd6b
clmb00 <paolo.colombo2000@gmail.com>
2023-01-30 15:34:07 +0100

    [fix] disabled link in pagination

--- resources/js/components/PaginationNav.vue
+++ resources/js/components/PaginationNav.vue
@@ -17,7 +17,7 @@ export default{
 <template>
 
 <div class="link_container">
-  <div class="link" v-for="(link, i) in links" v-show="link.url" :key="i" :class="link.active ? 'active' : ''" v-html="link.label" @click="newPage(link.url)"></div>
+  <button class="link" v-for="(link, i) in links" :disabled="!link.url || link.active" :key="i" :class="link.active ? 'active' : ''" v-html="link.label" @click="newPage(link.url)"></button>
 </div>
 
 </template>
@@ -30,14 +30,18 @@ export default{
 }
 
 .link{
-  cursor: pointer;
   color: black;
   font-weight: bold;
+  cursor: pointer;
   padding: 1rem;
   background-color: rgba($color: #d3d3d3, $alpha: .7);
   border-radius: 1rem;
+  &:disabled{
+    background-color: rgba($color: #d3d3d3, $alpha: .4);
+    border-color: rgba($color: #d3d3d3, $alpha: .1);
+  }
   &.active{
-    background-color: rgba($color: #d3d3d3, $alpha: 1);
+    background-color: rgba($color: #d3d3d3, $alpha: 1.2);
   }
 }
 
